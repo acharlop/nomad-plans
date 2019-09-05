9933b4da36ed508eedca3f846aaaf667594a13f0
Avi Charlop <avicharlop@gmail.com>
2019-09-05 16:33:26 +0300

    firebase config and export

---
+++ services/firebase.js
@@ -0,0 +1,15 @@
+import * as firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/firestore'
+import 'firebase/database'
+import config from '~/config'
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(config.firebase)
+}
+
+export const FacebookAuthProvider = new firebase.auth.FacebookAuthProvider()
+export const auth = firebase.auth()
+export const DB = firebase.database()
+export const StoreDB = firebase.firestore()
+export default firebase
