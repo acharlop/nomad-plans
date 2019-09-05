1ab341509477f053d7ad09a99af79aa63af7be2d
Avi Charlop <avicharlop@gmail.com>
2019-09-05 10:38:39 +0300

    disable lint on hot reload

--- nuxt.config.js
+++ nuxt.config.js
@@ -41,7 +41,7 @@ module.exports = {
    */
   buildModules: [
     // Doc: https://github.com/nuxt-community/eslint-module
-    '@nuxtjs/eslint-module',
+    // '@nuxtjs/eslint-module',
     '@nuxtjs/vuetify',
   ],
   /*
