27876d777ef05bf06a7f70311e73df13bfe0fd69
Avi Charlop <avicharlop@gmail.com>
2019-09-06 01:40:30 +0300

    landing page redirect on authenticated

--- pages/landing/landing.page.js
+++ pages/landing/landing.page.js
@@ -8,10 +8,31 @@ export default {
   props: [],
   data() {
     return {
+      // TODO remove if making our own button
       facebookAppId: config.facebook.secret,
     }
   },
   computed: {},
-  mounted() {},
-  methods: {},
+  created() {
+    this.$store
+      .dispatch('auth/signInAutomatic')
+      .then((loggedIn) => {
+        if (loggedIn) this.$router.push('/')
+      })
+      .catch((e) => {
+        console.error(e)
+      })
+  },
+  methods: {
+    facebookLogin() {
+      this.$store
+        .dispatch('auth/signInWithFacebook')
+        .then(() => {
+          this.$router.push('/')
+        })
+        .catch((e) => {
+          console.error(e)
+        })
+    },
+  },
 }
