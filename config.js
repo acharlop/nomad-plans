bc5aa7e78ef3257cff2148bc1b0f2471af9e896e
Avi Charlop <avicharlop@gmail.com>
2019-09-06 11:20:03 +0300

    some more env

--- config.js
+++ config.js
@@ -1,24 +1,20 @@
-const env = require('dotenv').config()
-
-const base = env.error ? process.env : env.parsed
-
-const config = {
+const createConfig = () => ({
   googleMaps: {
-    secret: base.GOOGLE_MAPS_API_KEY,
+    secret: process.env.GOOGLE_MAPS_API_KEY,
   },
   facebook: {
-    secret: base.FACEBOOK_APP_ID,
+    secret: process.env.FACEBOOK_APP_ID,
   },
   firebase: {
-    apiKey: base.FIREBASE_API_KEY,
-    authDomain: base.FIREBASE_AUTH_DOMAIN,
-    databaseURL: base.FIREBASE_DATABASE_URL,
-    projectId: base.FIREBASE_PROJECT_ID,
-    storageBucket: base.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: base.FIREBASE_MESSAGING_SENDER_ID,
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
   },
-}
+})
 
+const config = createConfig()
 console.log(config)
-
 export default config
