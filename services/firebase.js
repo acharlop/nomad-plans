3fd66a4d6f855b1f703245a1576396cf8bcfc2e4
Avi Charlop <avicharlop@gmail.com>
2019-09-06 11:33:08 +0300

    manually setting

--- services/firebase.js
+++ services/firebase.js
@@ -2,10 +2,26 @@ import * as firebase from 'firebase/app'
 import 'firebase/auth'
 import 'firebase/firestore'
 import 'firebase/database'
-import config from '~/config'
-console.log(JSON.stringify(config, null, 2))
-console.log(process)
-console.log(process.env)
+
+const config = {
+  googleMaps: {
+    secret: process.env.GOOGLE_MAPS_API_KEY,
+  },
+  facebook: {
+    secret: process.env.FACEBOOK_APP_ID,
+  },
+  firebase: {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  },
+}
+
+console.log(config)
+
 if (!firebase.apps.length) {
   firebase.initializeApp(config.firebase)
 }
