79b69bf26594b076fd2c4732cf1792ac709c02b6
imostovenko <irina.mostovenko@gmail.com>
2019-09-09 00:37:26 +0300

    color schema and app, footer colors

---
+++ assets/vuetify.options.js
@@ -0,0 +1,21 @@
+const colors = require('vuetify/es5/util/colors').default
+
+export default {
+  theme: {
+    dark: true,
+    options: {
+      customProperties: true,
+    },
+    themes: {
+      dark: {
+        primary: colors.lightGreen.base,
+        primaryLight: '#bef67a',
+        primaryDark: '#5a9216',
+
+        secondary: colors.amber.base,
+        secondaryLight: '#fff350',
+        secondaryDark: '#c79100',
+      },
+    },
+  },
+}
