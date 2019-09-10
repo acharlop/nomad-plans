fde8d60a26b2e930e7179776a65b1fa6c025e1fa
Avi Charlop <avicharlop@gmail.com>
2019-09-10 12:31:36 +0300

    first pass at layout for cta on landing page

--- pages/landing/landing.page.js
+++ pages/landing/landing.page.js
@@ -1,13 +1,17 @@
 import Vue from 'vue'
 import AuthButton from '@/components/auth-button'
+import MapSvg from '@/assets/svgs/world.svg'
 
 export default Vue.component('LandingPageComponent', {
   components: {
     AuthButton,
+    MapSvg,
   },
   props: [],
   data() {
-    return {}
+    return {
+      signUp: false,
+    }
   },
   computed: {},
   created() {},
