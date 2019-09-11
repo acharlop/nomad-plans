000f72bdf351135e9603b9beab0f4b904ffbefb1
Avi Charlop <avicharlop@gmail.com>
2019-09-12 00:33:24 +0300

    card-plan simple display and toggle friends and text

---
+++ components/card-plan/card-plan.component.spec.js
@@ -0,0 +1,32 @@
+import { mount } from '@vue/test-utils'
+import CardPlanComponentComponent from './index.vue'
+
+describe('CardPlanComponentComponent', () => {
+  // is vue component
+  test('is Vue component', () => {
+    const wrapper = mount(CardPlanComponentComponent)
+    expect(wrapper.isVueInstance()).toBeTruthy()
+  })
+  // Inspect the raw component options
+  test('has a created hook', () => {
+    // expect(typeof CardPlanComponentComponent.created).toBe('function');
+  })
+  // Evaluate the results of functions in
+  // the raw component options
+  test('sets the correct default data', () => {
+    // expect(typeof CardPlanComponentComponent.data).toBe('function')
+    // const defaultData = CardPlanComponentComponent.data();
+    // expect(defaultData.message).toBe('hello!');
+  })
+  // Inspect the component instance on mount
+  test('correctly sets the message when created', () => {
+    // const vm = new Vue(CardPlanComponentComponent).$mount();
+    // expect(vm.message).toBe('bye!');
+  })
+  // Mount an instance and inspect the render output
+  test('renders the correct message', () => {
+    // const Ctor = Vue.extend(CardPlanComponentComponent);
+    // const vm = new Ctor().$mount();
+    // expect(vm.$el.textContent).toBe('bye!');
+  })
+})
