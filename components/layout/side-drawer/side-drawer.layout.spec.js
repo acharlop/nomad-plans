c81db8e40d95847ad2826ac26e29b6a1774ada79
Avi Charlop <avicharlop@gmail.com>
2019-09-23 20:48:15 +0200

    some testing

--- components/layout/side-drawer/side-drawer.layout.spec.js
+++ components/layout/side-drawer/side-drawer.layout.spec.js
@@ -2,19 +2,25 @@ import SideDrawerLayoutComponent from './index.vue'
 import { mount } from '@/test/test-utils'
 
 let storeOptions
+let toggleSideDrawer
+let toggleSideDrawerTab
 
 describe('SideDrawerLayoutComponent', () => {
   beforeEach(() => {
+    toggleSideDrawer = jest.fn()
+    toggleSideDrawerTab = jest.fn()
+
     storeOptions = {
       modules: {
         layout: {
+          namespaced: true,
           state: {
             showSideDrawer: true,
             sideDrawerTab: 'mine',
           },
           mutations: {
-            toggleSideDrawer: jest.fn(),
-            toggleSideDrawerTab: jest.fn(),
+            toggleSideDrawer,
+            toggleSideDrawerTab,
           },
         },
         plans: {
@@ -36,26 +42,16 @@ describe('SideDrawerLayoutComponent', () => {
     const wrapper = mount(SideDrawerLayoutComponent, { storeOptions })
     expect(wrapper.isVueInstance()).toBeTruthy()
   })
-  // Inspect the raw component options
-  test('has a created hook', () => {
-    // expect(typeof SideDrawerLayoutComponent.created).toBe('function');
+
+  test('toggles visibility', () => {
+    const wrapper = mount(SideDrawerLayoutComponent, { storeOptions })
+    wrapper.setData({ visible: false })
+    expect(toggleSideDrawer).toHaveBeenCalled()
   })
-  // Evaluate the results of functions in
-  // the raw component options
-  test('sets the correct default data', () => {
-    // expect(typeof SideDrawerLayoutComponent.data).toBe('function')
-    // const defaultData = SideDrawerLayoutComponent.data();
-    // expect(defaultData.message).toBe('hello!');
-  })
-  // Inspect the component instance on mount
-  test('correctly sets the message when created', () => {
-    // const vm = new Vue(SideDrawerLayoutComponent).$mount();
-    // expect(vm.message).toBe('bye!');
-  })
-  // Mount an instance and inspect the render output
-  test('renders the correct message', () => {
-    // const Ctor = Vue.extend(SideDrawerLayoutComponent);
-    // const vm = new Ctor().$mount();
-    // expect(vm.$el.textContent).toBe('bye!');
+
+  test('toggles selected tab', () => {
+    const wrapper = mount(SideDrawerLayoutComponent, { storeOptions })
+    wrapper.setData({ selectedTab: 'friends' })
+    expect(toggleSideDrawerTab).toHaveBeenCalled()
   })
 })
