13a07efb0a84f82fb0bc58659220a8c57b141ab0
Avi Charlop <avicharlop@gmail.com>
2019-09-24 16:14:46 +0100

    close sidebar on mobile to larger screen

--- layouts/main/main.layout.js
+++ layouts/main/main.layout.js
@@ -17,17 +17,27 @@ export default Vue.component('MainLayout', {
   },
   props: [],
   data() {
-    return {}
+    return {
+      currentWidth: window.innerWidth,
+    }
   },
   computed: {
     ...mapState({
       legalDialog: (state) => state.layout.dialogs.legal,
       inviteDialog: (state) => state.layout.dialogs.invite,
       planFormDialog: (state) => state.layout.dialogs.planForm,
+      showSideDrawer: (state) => state.layout.showSideDrawer,
     }),
   },
   mounted() {},
   methods: {
-    ...mapMutations('layout', ['closeDialogs']),
+    ...mapMutations('layout', ['closeDialogs', 'toggleSideDrawer']),
+    handleResize() {
+      if (this.currentWidth === window.innerWidth) return
+      if (this.showSideDrawer && window.innerWidth > 600) {
+        this.toggleSideDrawer()
+      }
+      this.currentWidth = window.innerWidth
+    },
   },
 })
