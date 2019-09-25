925920036a0870fd977dc9466fac30af86ebbe18
Avi Charlop <avicharlop@gmail.com>
2019-09-25 18:44:17 +0100

    date interval x days, x weeks, 2+ months

--- utils/date.js
+++ utils/date.js
@@ -1,9 +1,22 @@
 import addDays from 'date-fns/addDays'
-import formatDistance from 'date-fns/formatDistance'
+import formatDistanceStrict from 'date-fns/formatDistanceStrict'
+import differenceInMonths from 'date-fns/differenceInMonths'
+import differenceInWeeks from 'date-fns/differenceInWeeks'
 
 export const dateDistance = (start, end) => {
   if (!start || !end) return ''
 
+  const startDate = new Date(start)
   const fakeEnd = addDays(new Date(end), 1)
-  return formatDistance(new Date(start), fakeEnd)
+
+  const weeks = differenceInWeeks(fakeEnd, startDate)
+  const is2months = differenceInMonths(fakeEnd, startDate) > 1
+
+  if (weeks && !is2months) {
+    return `${weeks} week${weeks > 1 ? 's' : ''}`
+  }
+
+  const unit = is2months ? 'month' : 'day'
+
+  return formatDistanceStrict(startDate, fakeEnd, { unit })
 }
