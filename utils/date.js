f43a439ad2a6053328dd2e5398961d6d141169f1
Avi Charlop <avicharlop@gmail.com>
2020-01-23 22:01:47 -0400

    date format consts, change dates to parseISO, migrate formatRange from card component

--- utils/date.js
+++ utils/date.js
@@ -3,18 +3,20 @@ import formatDistanceStrict from 'date-fns/formatDistanceStrict'
 import differenceInMonths from 'date-fns/differenceInMonths'
 import differenceInWeeks from 'date-fns/differenceInWeeks'
 import DFNisWithinInterval from 'date-fns/isWithinInterval'
-import { minTime, maxTime } from 'date-fns/constants'
-import lightFormat from 'date-fns/lightFormat'
 import DFNformat from 'date-fns/format'
 import parseISO from 'date-fns/parseISO'
+import isSameYear from 'date-fns/isSameYear'
+
+export const DATE_FORMAT_NUMS = 'yyyy-MM-dd'
+export const DATE_FORMAT_WORD = 'MMM d, yyyy'
 
 // TODO [structure] split each function into it's own file
 
 export const formatDistance = (start, end) => {
   if (!start || !end) return ''
 
-  const startDate = new Date(start)
-  const fakeEnd = addDays(new Date(end), 1)
+  const startDate = parseISO(start)
+  const fakeEnd = addDays(parseISO(end), 1)
 
   const weeks = differenceInWeeks(fakeEnd, startDate)
   const is2months = differenceInMonths(fakeEnd, startDate) > 1
@@ -28,21 +30,33 @@ export const formatDistance = (start, end) => {
   return formatDistanceStrict(startDate, fakeEnd, { unit })
 }
 
+export const formatRange = (start, end) => {
+  const startAt = parseISO(start)
+  const endAt = parseISO(end)
+
+  const startFormat = isSameYear(startAt, endAt) ? 'MMM d' : DATE_FORMAT_WORD
+
+  return `${DFNformat(startAt, startFormat)} - ${DFNformat(
+    endAt,
+    DATE_FORMAT_WORD
+  )}`
+}
+
 export const dayInPlan = (day, plan) => {
   const { startAt, endAt } = plan
 
   return !day || !startAt || !endAt
     ? false
-    : DFNisWithinInterval(new Date(day), {
-        start: new Date(startAt),
-        end: new Date(endAt),
+    : DFNisWithinInterval(parseISO(day), {
+        start: parseISO(startAt),
+        end: parseISO(endAt),
       })
 }
 
 export const isWithinInterval = (day, range) => {
-  return DFNisWithinInterval(new Date(day), {
-    start: new Date(range.startAt),
-    end: new Date(range.endAt),
+  return DFNisWithinInterval(parseISO(day), {
+    start: parseISO(range.startAt),
+    end: parseISO(range.endAt),
   })
 }
 
@@ -50,48 +64,7 @@ export const isWithinAnyInterval = (day, ranges = []) => {
   return ranges.some((range) => isWithinInterval(day, range))
 }
 
-export const intervalContainingDate = (ranges = [], day) => {
-  let interval = { startAfter: '', endBefore: '' }
-
-  // check for present ranges
-  if (!ranges.length) return interval
-
-  // constants
-  const max = new Date(maxTime)
-  const min = new Date(minTime)
-  const date = new Date(day)
-
-  // build array of valid ranges
-  const validRanges = [{ start: '', end: max }]
-  ranges.forEach((range) => {
-    validRanges[validRanges.length - 1].start = new Date(range.endAt)
-    validRanges.push({ start: '', end: new Date(range.startAt) })
-  })
-  validRanges[validRanges.length - 1].start = min
-
-  // find range
-  validRanges.some((range) => {
-    if (DFNisWithinInterval(date, range)) {
-      interval = { startAfter: range.start, endBefore: range.end }
-      return true
-    }
-    return false
-  })
-
-  let { startAfter, endBefore } = interval
-
-  startAfter =
-    !startAfter || startAfter === min
-      ? ''
-      : lightFormat(startAfter, 'yyyy-MM-dd')
-
-  endBefore =
-    !endBefore || endBefore === max ? '' : lightFormat(endBefore, 'yyyy-MM-dd')
-
-  return { startAfter, endBefore }
-}
-
-export const formatDate = (date = '', dateFormat = 'MMM dd, yyyy') => {
+export const formatDate = (date = '', dateFormat = DATE_FORMAT_WORD) => {
   if (date && dateFormat) {
     return DFNformat(parseISO(date), dateFormat)
   }
