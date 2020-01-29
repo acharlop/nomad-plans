3f843846f825261084287e919a8c7e2774f95c9d
Avi Charlop <avicharlop@gmail.com>
2020-01-29 00:27:22 -0400

    fix more tests

--- components/layout/footer/footer.layout.spec.js
+++ components/layout/footer/footer.layout.spec.js
@@ -1,10 +1,10 @@
 import FooterComponent from './index.vue'
-import { mount } from '@/test/test-utils'
+import { shallow } from '@/test/test-utils'
 
 describe('FooterComponent', () => {
   // is vue component
   test('is Vue component', () => {
-    const wrapper = mount(FooterComponent)
+    const wrapper = shallow(FooterComponent)
     expect(wrapper.isVueInstance()).toBeTruthy()
   })
 })
