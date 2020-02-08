31cdc30b6731bbd8a8d336eaa732af782a2e5cf1
Avi Charlop <avicharlop@gmail.com>
2020-02-08 11:45:37 -0400

    filters tests

--- components/filters/filters.component.spec.js
+++ components/filters/filters.component.spec.js
@@ -2,9 +2,14 @@ import Filters from './index.vue'
 import { mount } from '@/test/test-utils'
 
 let storeOptions
+let setUnconfirmedFilter
+let setPastFilter
 
 describe('FiltersComponent', () => {
   beforeEach(() => {
+    setUnconfirmedFilter = jest.fn()
+    setPastFilter = jest.fn()
+
     storeOptions = {
       modules: {
         plans: {
@@ -12,11 +17,12 @@ describe('FiltersComponent', () => {
           state: {
             filters: {
               hidePast: false,
+              hideUnconfirmed: false,
             },
           },
           mutations: {
-            setUnconfirmedFilter: jest.fn(),
-            setPastFilter: jest.fn(),
+            setPastFilter,
+            setUnconfirmedFilter,
           },
         },
       },
@@ -26,5 +32,35 @@ describe('FiltersComponent', () => {
   test('is Vue component', () => {
     const wrapper = mount(Filters, { storeOptions })
     expect(wrapper.isVueInstance()).toBeTruthy()
+  })
+
+  test('toggles past filter', () => {
+    const wrapper = mount(Filters, { storeOptions })
+    const checkbox = wrapper.findAll('input').at(1)
+    expect(wrapper.vm.past).toBeFalsy()
+
+    checkbox.trigger('click')
+
+    expect(wrapper.vm.past).toBeTruthy()
+    expect(setPastFilter).toHaveBeenCalledTimes(1)
+
+    checkbox.trigger('click')
+    expect(wrapper.vm.past).toBeFalsy()
+    expect(setPastFilter).toHaveBeenCalledTimes(2)
+  })
+
+  test('toggles unconfirmed filter', () => {
+    const wrapper = mount(Filters, { storeOptions })
+    const checkbox = wrapper.findAll('input').at(0)
+    expect(wrapper.vm.unconfirmed).toBeFalsy()
+
+    checkbox.trigger('click')
+
+    expect(wrapper.vm.unconfirmed).toBeTruthy()
+    expect(setUnconfirmedFilter).toHaveBeenCalledTimes(1)
+
+    checkbox.trigger('click')
+    expect(wrapper.vm.unconfirmed).toBeFalsy()
+    expect(setUnconfirmedFilter).toHaveBeenCalledTimes(2)
   })
 })
